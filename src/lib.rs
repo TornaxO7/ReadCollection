5248ff3f2f17507a88060dfa27c90af8869e3232
TornaxO7 <tornax@proton.me>
2024-04-21 15:08:01 +0200

    implement rev_borrowed_buf

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 #![feature(core_io_borrowed_buf)]
 #![feature(new_uninit)]
 #![feature(maybe_uninit_slice)]
+#![feature(maybe_uninit_write_slice)]
 #![feature(read_buf)]
+#![feature(ptr_as_uninit)]
 
 mod bibufreader;
+mod rev_borrowed_buf;
 mod rev_buf_reader;
 mod rev_reader;
 
 pub use bibufreader::BiBufReader;
+pub use rev_borrowed_buf::RevBorrowedBuf;
 pub use rev_buf_reader::RevBufRead;
 pub use rev_reader::RevRead;
