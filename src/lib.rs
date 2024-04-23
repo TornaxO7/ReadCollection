9e3f71fc25ea4e164cf84ae36fefc4bf376a6976
TornaxO7 <tornax@proton.me>
2024-04-23 09:09:54 +0200

    restructure the crate

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,13 @@
 #![feature(ptr_as_uninit)]
 
 // mod bibufreader;
+// mod rev_borrowed_buf;
+// mod rev_buf_reader;
+// mod rev_reader;
+
+mod impls;
+mod rev;
 mod rev_borrowed_buf;
-mod rev_buf_reader;
-mod rev_reader;
 
 // Bare metal platforms usually have very small amounts of RAM
 // (in the order of hundreds of KB)
@@ -19,6 +23,5 @@ pub const DEFAULT_BUF_SIZE: usize = if cfg!(target_os = "espidf") {
 };
 
 // pub use bibufreader::BiBufReader;
+pub use rev::{RevBufRead, RevRead};
 pub use rev_borrowed_buf::RevBorrowedBuf;
-pub use rev_buf_reader::RevBufRead;
-pub use rev_reader::RevRead;
---
+++ src/rev.rs
@@ -0,0 +1,672 @@
+use std::{
+    io::{self, ErrorKind, IoSliceMut, Result, Take},
+    slice,
+};
+
+use crate::{rev_borrowed_buf::RevBorrowedCursor, RevBorrowedBuf, DEFAULT_BUF_SIZE};
+
+/// Equals the [std::io::Read] trait, except that everything is in reverse.
+pub trait RevRead {
+    fn rev_read(&mut self, buf: &mut [u8]) -> Result<usize>;
+
+    fn rev_read_vectored(&mut self, bufs: &mut [IoSliceMut<'_>]) -> Result<usize> {
+        default_rev_read_vectored(|b| self.rev_read(b), bufs)
+    }
+    fn rev_is_read_vectored(&self) -> bool {
+        false
+    }
+    fn rev_read_to_end(&mut self, buf: &mut Vec<u8>) -> Result<usize> {
+        default_rev_read_to_end(self, buf, None)
+    }
+    fn rev_read_to_string(&mut self, buf: &mut String) -> Result<usize> {
+        default_rev_read_to_string(self, buf, None)
+    }
+    fn rev_read_exact(&mut self, buf: &mut [u8]) -> Result<()> {
+        default_rev_read_exact(self, buf)
+    }
+    fn rev_read_buf(&mut self, buf: RevBorrowedCursor<'_>) -> Result<()> {
+        default_rev_read_buf(|b| self.rev_read(b), buf)
+    }
+    fn rev_read_buf_exact(&mut self, cursor: RevBorrowedCursor<'_>) -> Result<()> {
+        default_rev_read_buf_exact(self, cursor)
+    }
+    fn rev_by_ref(&mut self) -> &mut Self
+    where
+        Self: Sized,
+    {
+        self
+    }
+    fn rev_bytes(self) -> Bytes<Self>
+    where
+        Self: Sized,
+    {
+        Bytes { inner: self }
+    }
+    fn rev_chain<R: io::Read>(self, next: R) -> Chain<Self, R>
+    where
+        Self: Sized,
+    {
+        Chain {
+            first: self,
+            second: next,
+            done_first: false,
+        }
+    }
+    fn rev_take(self, limit: u64) -> Take<Self>
+    where
+        Self: Sized,
+    {
+        todo!("Can't directly use from std");
+    }
+}
+
+/// Equals the [std::io::BufRead] trait, except that everything is in reverse.
+pub trait RevBufRead: RevRead {
+    fn rev_fill_buf(&mut self) -> io::Result<&[u8]>;
+
+    fn rev_consume(&mut self, amt: usize);
+
+    // Provided methods
+    fn rev_has_data_left(&mut self) -> io::Result<bool> {
+        todo!()
+    }
+
+    fn rev_read_until(&mut self, byte: u8, buf: &mut Vec<u8>) -> io::Result<usize> {
+        todo!()
+    }
+
+    fn rev_skip_until(&mut self, byte: u8) -> io::Result<usize> {
+        todo!()
+    }
+
+    fn rev_read_line(&mut self, buf: &mut String) -> io::Result<usize> {
+        todo!()
+    }
+
+    fn rev_split(self, byte: u8) -> Split<Self>
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+
+    fn rev_lines(self) -> Lines<Self>
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+}
+
+/// An iterator over `u8` values of a reader.
+///
+/// This struct is generally created by calling [`bytes`] on a reader.
+/// Please see the documentation of [`bytes`] for more details.
+///
+/// [`bytes`]: Read::bytes
+#[derive(Debug)]
+pub struct Bytes<R> {
+    pub inner: R,
+}
+
+impl<R: RevRead> Iterator for Bytes<R> {
+    type Item = Result<u8>;
+
+    // Not `#[inline]`. This function gets inlined even without it, but having
+    // the inline annotation can result in worse code generation. See #116785.
+    fn next(&mut self) -> Option<Result<u8>> {
+        SpecReadByte::spec_read_byte(&mut self.inner)
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        SizeHint::size_hint(&self.inner)
+    }
+}
+
+/// For the specialization of `Bytes::next`.
+trait SpecReadByte {
+    fn spec_read_byte(&mut self) -> Option<Result<u8>>;
+}
+
+impl<R> SpecReadByte for R
+where
+    Self: RevRead,
+{
+    #[inline]
+    fn spec_read_byte(&mut self) -> Option<Result<u8>> {
+        inlined_slow_read_byte(self)
+    }
+}
+
+trait SizeHint {
+    fn lower_bound(&self) -> usize;
+
+    fn upper_bound(&self) -> Option<usize>;
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.lower_bound(), self.upper_bound())
+    }
+}
+
+impl<T: ?Sized> SizeHint for T {
+    #[inline]
+    fn lower_bound(&self) -> usize {
+        0
+    }
+
+    #[inline]
+    fn upper_bound(&self) -> Option<usize> {
+        None
+    }
+}
+
+// impl<T> SizeHint for &mut T {
+//     #[inline]
+//     fn lower_bound(&self) -> usize {
+//         SizeHint::lower_bound(*self)
+//     }
+
+//     #[inline]
+//     fn upper_bound(&self) -> Option<usize> {
+//         SizeHint::upper_bound(*self)
+//     }
+// }
+
+// impl<T> SizeHint for Box<T> {
+//     #[inline]
+//     fn lower_bound(&self) -> usize {
+//         SizeHint::lower_bound(&**self)
+//     }
+
+//     #[inline]
+//     fn upper_bound(&self) -> Option<usize> {
+//         SizeHint::upper_bound(&**self)
+//     }
+// }
+
+// impl SizeHint for &[u8] {
+//     #[inline]
+//     fn lower_bound(&self) -> usize {
+//         self.len()
+//     }
+
+//     #[inline]
+//     fn upper_bound(&self) -> Option<usize> {
+//         Some(self.len())
+//     }
+// }
+
+/// Read a single byte in a slow, generic way. This is used by the default
+/// `spec_read_byte`.
+#[inline]
+fn inlined_slow_read_byte<R: RevRead>(reader: &mut R) -> Option<Result<u8>> {
+    let mut byte = 0;
+    loop {
+        return match reader.rev_read(slice::from_mut(&mut byte)) {
+            Ok(0) => None,
+            Ok(..) => Some(Ok(byte)),
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => Some(Err(e)),
+        };
+    }
+}
+
+/// Adapter to chain together two readers.
+///
+/// This struct is generally created by calling [`chain`] on a reader.
+/// Please see the documentation of [`chain`] for more details.
+///
+/// [`chain`]: Read::chain
+#[derive(Debug)]
+pub struct Chain<T, U> {
+    first: T,
+    second: U,
+    done_first: bool,
+}
+
+impl<T, U> Chain<T, U> {
+    /// Consumes the `Chain`, returning the wrapped readers.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::io;
+    /// use std::io::prelude::*;
+    /// use std::fs::File;
+    ///
+    /// fn main() -> io::Result<()> {
+    ///     let mut foo_file = File::open("foo.txt")?;
+    ///     let mut bar_file = File::open("bar.txt")?;
+    ///
+    ///     let chain = foo_file.chain(bar_file);
+    ///     let (foo_file, bar_file) = chain.into_inner();
+    ///     Ok(())
+    /// }
+    /// ```
+    pub fn into_inner(self) -> (T, U) {
+        (self.first, self.second)
+    }
+
+    /// Gets references to the underlying readers in this `Chain`.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::io;
+    /// use std::io::prelude::*;
+    /// use std::fs::File;
+    ///
+    /// fn main() -> io::Result<()> {
+    ///     let mut foo_file = File::open("foo.txt")?;
+    ///     let mut bar_file = File::open("bar.txt")?;
+    ///
+    ///     let chain = foo_file.chain(bar_file);
+    ///     let (foo_file, bar_file) = chain.get_ref();
+    ///     Ok(())
+    /// }
+    /// ```
+    pub fn get_ref(&self) -> (&T, &U) {
+        (&self.first, &self.second)
+    }
+
+    /// Gets mutable references to the underlying readers in this `Chain`.
+    ///
+    /// Care should be taken to avoid modifying the internal I/O state of the
+    /// underlying readers as doing so may corrupt the internal state of this
+    /// `Chain`.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::io;
+    /// use std::io::prelude::*;
+    /// use std::fs::File;
+    ///
+    /// fn main() -> io::Result<()> {
+    ///     let mut foo_file = File::open("foo.txt")?;
+    ///     let mut bar_file = File::open("bar.txt")?;
+    ///
+    ///     let mut chain = foo_file.chain(bar_file);
+    ///     let (foo_file, bar_file) = chain.get_mut();
+    ///     Ok(())
+    /// }
+    /// ```
+    pub fn get_mut(&mut self) -> (&mut T, &mut U) {
+        (&mut self.first, &mut self.second)
+    }
+}
+
+impl<T: RevRead, U: RevRead> RevRead for Chain<T, U> {
+    fn rev_read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        if !self.done_first {
+            match self.first.rev_read(buf)? {
+                0 if !buf.is_empty() => self.done_first = true,
+                n => return Ok(n),
+            }
+        }
+        self.second.rev_read(buf)
+    }
+
+    fn rev_read_vectored(&mut self, bufs: &mut [IoSliceMut<'_>]) -> Result<usize> {
+        if !self.done_first {
+            match self.first.rev_read_vectored(bufs)? {
+                0 if bufs.iter().any(|b| !b.is_empty()) => self.done_first = true,
+                n => return Ok(n),
+            }
+        }
+        self.second.rev_read_vectored(bufs)
+    }
+
+    #[inline]
+    fn rev_is_read_vectored(&self) -> bool {
+        self.first.rev_is_read_vectored() || self.second.rev_is_read_vectored()
+    }
+
+    fn rev_read_to_end(&mut self, buf: &mut Vec<u8>) -> Result<usize> {
+        let mut read = 0;
+        if !self.done_first {
+            read += self.first.rev_read_to_end(buf)?;
+            self.done_first = true;
+        }
+        read += self.second.rev_read_to_end(buf)?;
+        Ok(read)
+    }
+
+    // We don't override `read_to_string` here because an UTF-8 sequence could
+    // be split between the two parts of the chain
+
+    fn rev_read_buf(&mut self, mut buf: RevBorrowedCursor<'_>) -> Result<()> {
+        if buf.capacity() == 0 {
+            return Ok(());
+        }
+
+        if !self.done_first {
+            let old_len = buf.written();
+            self.first.rev_read_buf(buf.reborrow())?;
+
+            if buf.written() != old_len {
+                return Ok(());
+            } else {
+                self.done_first = true;
+            }
+        }
+        self.second.rev_read_buf(buf)
+    }
+}
+
+impl<T: RevBufRead, U: RevBufRead> RevBufRead for Chain<T, U> {
+    fn rev_fill_buf(&mut self) -> Result<&[u8]> {
+        if !self.done_first {
+            match self.first.rev_fill_buf()? {
+                buf if buf.is_empty() => self.done_first = true,
+                buf => return Ok(buf),
+            }
+        }
+        self.second.rev_fill_buf()
+    }
+
+    fn rev_consume(&mut self, amt: usize) {
+        if !self.done_first {
+            self.first.rev_consume(amt)
+        } else {
+            self.second.rev_consume(amt)
+        }
+    }
+
+    fn rev_read_until(&mut self, byte: u8, buf: &mut Vec<u8>) -> Result<usize> {
+        let mut read = 0;
+        if !self.done_first {
+            let n = self.first.rev_read_until(byte, buf)?;
+            read += n;
+
+            match buf.last() {
+                Some(b) if *b == byte && n != 0 => return Ok(read),
+                _ => self.done_first = true,
+            }
+        }
+        read += self.second.rev_read_until(byte, buf)?;
+        Ok(read)
+    }
+
+    // We don't override `read_line` here because an UTF-8 sequence could be
+    // split between the two parts of the chain
+}
+
+// impl<T, U> SizeHint for Chain<T, U> {
+//     #[inline]
+//     fn lower_bound(&self) -> usize {
+//         SizeHint::lower_bound(&self.first) + SizeHint::lower_bound(&self.second)
+//     }
+
+//     #[inline]
+//     fn upper_bound(&self) -> Option<usize> {
+//         match (
+//             SizeHint::upper_bound(&self.first),
+//             SizeHint::upper_bound(&self.second),
+//         ) {
+//             (Some(first), Some(second)) => first.checked_add(second),
+//             _ => None,
+//         }
+//     }
+// }
+
+/// An iterator over the contents of an instance of `BufRead` split on a
+/// particular byte.
+///
+/// This struct is generally created by calling [`split`] on a `BufRead`.
+/// Please see the documentation of [`split`] for more details.
+///
+/// [`split`]: BufRead::split
+#[derive(Debug)]
+pub struct Split<B> {
+    buf: B,
+    delim: u8,
+}
+
+impl<B: RevBufRead> Iterator for Split<B> {
+    type Item = Result<Vec<u8>>;
+
+    fn next(&mut self) -> Option<Result<Vec<u8>>> {
+        let mut buf = Vec::new();
+        match self.buf.rev_read_until(self.delim, &mut buf) {
+            Ok(0) => None,
+            Ok(_n) => {
+                if buf[buf.len() - 1] == self.delim {
+                    buf.pop();
+                }
+                Some(Ok(buf))
+            }
+            Err(e) => Some(Err(e)),
+        }
+    }
+}
+
+/// An iterator over the lines of an instance of `BufRead`.
+///
+/// This struct is generally created by calling [`lines`] on a `BufRead`.
+/// Please see the documentation of [`lines`] for more details.
+///
+/// [`lines`]: BufRead::lines
+#[derive(Debug)]
+pub struct Lines<B> {
+    buf: B,
+}
+
+impl<B: RevBufRead> Iterator for Lines<B> {
+    type Item = Result<String>;
+
+    fn next(&mut self) -> Option<Result<String>> {
+        let mut buf = String::new();
+        match self.buf.rev_read_line(&mut buf) {
+            Ok(0) => None,
+            Ok(_n) => {
+                if buf.ends_with('\n') {
+                    buf.pop();
+                    if buf.ends_with('\r') {
+                        buf.pop();
+                    }
+                }
+                Some(Ok(buf))
+            }
+            Err(e) => Some(Err(e)),
+        }
+    }
+}
+
+pub fn default_rev_read_vectored<F>(rev_read: F, bufs: &mut [IoSliceMut<'_>]) -> Result<usize>
+where
+    F: FnOnce(&mut [u8]) -> Result<usize>,
+{
+    let buf = bufs
+        .iter_mut()
+        .find(|b| !b.is_empty())
+        .map_or(&mut [][..], |b| &mut **b);
+    rev_read(buf)
+}
+
+pub fn default_rev_read_to_end<R: RevRead + ?Sized>(
+    r: &mut R,
+    buf: &mut Vec<u8>,
+    size_hint: Option<usize>,
+) -> Result<usize> {
+    let start_len = buf.len();
+    let start_cap = buf.capacity();
+    // Optionally limit the maximum bytes read on each iteration.
+    // This adds an arbitrary fiddle factor to allow for more data than we expect.
+    let mut max_read_size = size_hint
+        .and_then(|s| {
+            s.checked_add(1024)?
+                .checked_next_multiple_of(DEFAULT_BUF_SIZE)
+        })
+        .unwrap_or(DEFAULT_BUF_SIZE);
+
+    let mut initialized = 0; // Extra initialized bytes from previous loop iteration
+
+    const PROBE_SIZE: usize = 32;
+
+    fn small_probe_read<R: RevRead + ?Sized>(r: &mut R, buf: &mut Vec<u8>) -> Result<usize> {
+        let mut probe = [0u8; PROBE_SIZE];
+
+        loop {
+            match r.rev_read(&mut probe) {
+                Ok(n) => {
+                    // there is no way to recover from allocation failure here
+                    // because the data has already been read.
+                    buf.reverse();
+                    buf.extend_from_slice(&probe[..n]);
+                    buf.reverse();
+                    return Ok(n);
+                }
+                Err(ref e) if ErrorKind::Interrupted == e.kind() => continue,
+                Err(e) => return Err(e),
+            }
+        }
+    }
+
+    // avoid inflating empty/small vecs before we have determined that there's anything to read
+    if (size_hint.is_none() || size_hint == Some(0)) && buf.capacity() - buf.len() < PROBE_SIZE {
+        let read = small_probe_read(r, buf)?;
+
+        if read == 0 {
+            return Ok(0);
+        }
+    }
+
+    loop {
+        if buf.len() == buf.capacity() && buf.capacity() == start_cap {
+            // The buffer might be an exact fit. Let's read into a probe buffer
+            // and see if it returns `Ok(0)`. If so, we've avoided an
+            // unnecessary doubling of the capacity. But if not, append the
+            // probe buffer to the primary buffer and let its capacity grow.
+            let read = small_probe_read(r, buf)?;
+
+            if read == 0 {
+                return Ok(buf.len() - start_len);
+            }
+        }
+
+        if buf.len() == buf.capacity() {
+            // buf is full, need more space
+            buf.try_reserve(PROBE_SIZE)
+                .map_err(|_| ErrorKind::OutOfMemory)?;
+        }
+
+        let mut spare = buf.spare_capacity_mut();
+        let buf_len = std::cmp::min(spare.len(), max_read_size);
+        spare = &mut spare[..buf_len];
+        let mut read_buf: RevBorrowedBuf<'_> = spare.into();
+
+        // SAFETY: These bytes were initialized but not filled in the previous loop
+        unsafe {
+            read_buf.set_init(initialized);
+        }
+
+        let mut cursor = read_buf.unfilled();
+        loop {
+            match r.rev_read_buf(cursor.reborrow()) {
+                Ok(()) => break,
+                Err(e) if e.kind() == ErrorKind::Interrupted => continue,
+                Err(e) => return Err(e),
+            }
+        }
+
+        let unfilled_but_initialized = cursor.init_ref().len();
+        let bytes_read = cursor.written();
+        let was_fully_initialized = read_buf.init_len() == buf_len;
+
+        if bytes_read == 0 {
+            return Ok(buf.len() - start_len);
+        }
+
+        // store how much was initialized but not filled
+        initialized = unfilled_but_initialized;
+
+        // SAFETY: BorrowedBuf's invariants mean this much memory is initialized.
+        unsafe {
+            let new_len = bytes_read + buf.len();
+            buf.set_len(new_len);
+        }
+
+        // Use heuristics to determine the max read size if no initial size hint was provided
+        if size_hint.is_none() {
+            // The reader is returning short reads but it doesn't call ensure_init().
+            // In that case we no longer need to restrict read sizes to avoid
+            // initialization costs.
+            if !was_fully_initialized {
+                max_read_size = usize::MAX;
+            }
+
+            // we have passed a larger buffer than previously and the
+            // reader still hasn't returned a short read
+            if buf_len >= max_read_size && bytes_read == buf_len {
+                max_read_size = max_read_size.saturating_mul(2);
+            }
+        }
+    }
+}
+
+pub fn default_rev_read_to_string<R: RevRead + ?Sized>(
+    r: &mut R,
+    buf: &mut String,
+    size_hint: Option<usize>,
+) -> Result<usize> {
+    todo!()
+}
+
+pub fn default_rev_read_exact<R: RevRead + ?Sized>(this: &mut R, mut buf: &mut [u8]) -> Result<()> {
+    while !buf.is_empty() {
+        match this.rev_read(buf) {
+            Ok(0) => break,
+            Ok(n) => {
+                let buf_len = buf.len();
+                buf = &mut buf[..buf_len - n];
+            }
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => {}
+            Err(e) => return Err(e),
+        }
+    }
+    if !buf.is_empty() {
+        Err(std::io::Error::new(
+            ErrorKind::UnexpectedEof,
+            "failed to fill whole buffer",
+        ))
+    } else {
+        Ok(())
+    }
+}
+
+pub fn default_rev_read_buf<F>(read: F, mut cursor: RevBorrowedCursor<'_>) -> Result<()>
+where
+    F: FnOnce(&mut [u8]) -> Result<usize>,
+{
+    let n = read(cursor.ensure_init().init_mut())?;
+    unsafe {
+        // SAFETY: we initialised using `ensure_init` so there is no uninit data to advance to.
+        cursor.advance(n);
+    }
+    Ok(())
+}
+
+pub fn default_rev_read_buf_exact<R: RevRead + ?Sized>(
+    read: &mut R,
+    mut cursor: RevBorrowedCursor<'_>,
+) -> Result<()> {
+    while cursor.capacity() > 0 {
+        let prev_written = cursor.written();
+        match read.rev_read_buf(cursor.reborrow()) {
+            Ok(()) => {}
+            Err(e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e),
+        }
+
+        if cursor.written() == prev_written {
+            return Err(std::io::Error::new(
+                ErrorKind::UnexpectedEof,
+                "failed to fill buffer",
+            ));
+        }
+    }
+
+    Ok(())
+}
