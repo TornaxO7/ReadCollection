f67740e276ccea7d44cb4742bd4fff6de2fb0355
TornaxO7 <tornax@proton.me>
2024-06-11 01:23:33 +0200

    adding chain test

--- tests/file/same_as_read.rs
+++ tests/file/same_as_read.rs
@@ -5,13 +5,17 @@ use std::{
 
 use rev_read::{RevBorrowedBuf, RevRead};
 
-fn get_file() -> File {
-    File::open("./tests/file/test_file.txt").unwrap()
+fn get_file1() -> File {
+    File::open("./tests/file/test_file1.txt").unwrap()
+}
+
+fn get_file2() -> File {
+    File::open("./tests/file/test_file2.txt").unwrap()
 }
 
 #[test]
 fn read_vs_rev_read() {
-    let mut file = get_file();
+    let mut file = get_file1();
 
     let mut read_buffer = [0u8; 5];
     let mut rev_read_buffer = read_buffer.clone();
@@ -24,7 +28,7 @@ fn read_vs_rev_read() {
 
 #[test]
 fn read_to_end_vs_rev_read_to_end() {
-    let mut file = get_file();
+    let mut file = get_file1();
 
     let mut read_buffer = Vec::new();
     let mut rev_read_buffer = Vec::new();
@@ -44,7 +48,7 @@ fn read_to_end_vs_rev_read_to_end() {
 
 #[test]
 fn read_to_string_vs_rev_read_to_string() {
-    let mut file = get_file();
+    let mut file = get_file1();
 
     let mut read_buffer = String::new();
     let mut rev_read_buffer = String::new();
@@ -58,7 +62,7 @@ fn read_to_string_vs_rev_read_to_string() {
 
 #[test]
 fn read_exact_vs_rev_read_exact() {
-    let mut file = get_file();
+    let mut file = get_file1();
 
     let mut read_buffer: [u8; 10] = [0; 10];
     let mut rev_read_buffer: [u8; 10] = [0; 10];
@@ -71,7 +75,7 @@ fn read_exact_vs_rev_read_exact() {
 
 #[test]
 fn read_buf_vs_rev_read_buf() {
-    let mut file = get_file();
+    let mut file = get_file1();
 
     let mut read_buffer: Vec<u8> = Vec::new();
     let mut rev_read_buffer: Vec<u8> = Vec::new();
@@ -90,13 +94,38 @@ fn read_buf_vs_rev_read_buf() {
 
 #[test]
 fn read_bytes_vs_rev_read_bytes() {
-    let file = get_file();
+    let file = get_file1();
     let mut file2 = file.try_clone().unwrap();
     file2.seek(std::io::SeekFrom::End(0)).unwrap();
 
     let read_buffer = file.bytes().map(|b| b.unwrap()).collect::<Vec<u8>>();
     todo!("Issue: If the cursor reached the start of the file => How do we differ between the first time we reach there and 'ok, we've read all bytes now'?");
     let rev_read_buffer = file2.rev_bytes().map(|b| b.unwrap()).collect::<Vec<u8>>();
+
+    assert_eq!(read_buffer, rev_read_buffer);
+}
+
+#[test]
+fn read_chain_vs_rev_read_chain() {
+    let read_file1 = get_file1();
+    let read_file2 = get_file2();
+
+    let mut rev_read_file1 = get_file1();
+    let mut rev_read_file2 = get_file2();
+
+    rev_read_file1.seek(std::io::SeekFrom::End(0)).unwrap();
+    rev_read_file2.seek(std::io::SeekFrom::End(0)).unwrap();
+
+    let mut read_chain = read_file1.chain(read_file2);
+    let mut rev_read_chain = rev_read_file2.rev_chain(rev_read_file1);
+
+    let mut read_buffer = Vec::new();
+    let mut rev_read_buffer = Vec::new();
+
+    read_chain.read_to_end(&mut read_buffer).unwrap();
+    rev_read_chain
+        .rev_read_to_end(&mut rev_read_buffer)
+        .unwrap();
 
     assert_eq!(read_buffer, rev_read_buffer);
 }
