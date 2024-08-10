50079d8c0b7acee05f7eeb71b41568aebe8a2393
TornaxO7 <tornax@proton.me>
2024-08-10 11:45:52 +0200

    update examples

--- README.md
+++ README.md
@@ -9,16 +9,16 @@ use std::io::Read;
 
 fn main() {
     let values = [1, 2, 3];
-    let mut buffer = [0];
+    let mut buffer = [0, 0];
 
     // How it could look like with `Read`:
-    assert_eq!(values.as_slice().read(&mut buffer).ok(), Some(1));
-    assert_eq!(buffer, [1]);
+    assert_eq!(values.as_slice().read(&mut buffer).ok(), Some(2));
+    assert_eq!(buffer, [1, 2]);
 
     // With `ReadBack`:
-    assert_eq!(values.as_slice().read_back(&mut buffer).ok(), Some(1));
-    //                 [-] and the buffer contains the value starting from the back!
-    assert_eq!(buffer, [3]);
+    assert_eq!(values.as_slice().read_back(&mut buffer).ok(), Some(2));
+    //                 [----] and the buffer contains the value starting from the back!
+    assert_eq!(buffer, [2, 3]);
 }
 ```
 
--- examples/u8_slice.rs
+++ examples/u8_slice.rs
@@ -3,17 +3,17 @@ use std::io::Read;
 
 fn main() {
     let values = [1, 2, 3];
-    let mut buffer = [0];
+    let mut buffer = [0, 0];
 
     // How it could look like with `Read`:
-    assert_eq!(values.as_slice().read(&mut buffer).ok(), Some(1));
-    assert_eq!(buffer, [1]);
-    println!("With Read: buffer = [{}]", buffer[0]);
+    assert_eq!(values.as_slice().read(&mut buffer).ok(), Some(2));
+    assert_eq!(buffer, [1, 2]);
+    println!("With Read: buffer = [{}, {}]", buffer[0], buffer[1]);
 
     // The readback version:
-    //                           [--] <- notice the `rev_` here
-    assert_eq!(values.as_slice().read_back(&mut buffer).ok(), Some(1));
-    //                 [-] and the buffer contains the value starting from the back!
-    assert_eq!(buffer, [3]);
-    println!("With ReadBack: buffer = [{}]", buffer[0]);
+    //                               [----] <- notice the `_back` here
+    assert_eq!(values.as_slice().read_back(&mut buffer).ok(), Some(2));
+    //                 [----] and the buffer contains the value starting from the back!
+    assert_eq!(buffer, [2, 3]);
+    println!("With ReadBack: buffer = [{}, {}]", buffer[0], buffer[1]);
 }
